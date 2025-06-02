22f46f7831274e576cdf64aae21aaaeba12867d3
ugmniBal <microsoftbing027@gmail.com>
2025-06-02 22:07:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let data = [];
 let suggestionsEl = document.getElementById('suggestions');
+let currentList = [];
 
 fetch('merged_data.json')
   .then(res => res.json())
   .then(json => { data = json });
 
+document.getElementById('locationInput').addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    e.preventDefault(); // 자동완성 선택 안했으면 Enter 막기
+  }
+});
+
 function autocomplete() {
-  const input = document.getElementById("locationInput").value.trim();
+  const input = document.getElementById("locationInput").value.trim().replace(/\s+/g, '');
   suggestionsEl.innerHTML = "";
   if (!input) return;
-  const matched = data.filter(d => {
-    const full = `${d.시도명} ${d.시군구명} ${d.관리구역명}`;
+  currentList = data.filter(d => {
+    const full = \`\${d.시도명}\${d.시군구명}\${d.관리구역명}\`.replace(/\s+/g, '');
     return full.includes(input);
   });
-  matched.slice(0, 10).forEach(item => {
+  currentList.slice(0, 10).forEach(item => {
     const li = document.createElement("li");
-    li.textContent = `${item.시도명} ${item.시군구명} ${item.관리구역명}`;
+    li.textContent = \`\${item.시도명} \${item.시군구명} \${item.관리구역명}\`;
     li.onclick = () => {
       document.getElementById("locationInput").value = li.textContent;
       suggestionsEl.innerHTML = "";
@@ -25,19 +32,19 @@ function autocomplete() {
 }
 
 function searchInfo() {
-  const input = document.getElementById("locationInput").value.trim();
+  const input = document.getElementById("locationInput").value.trim().replace(/\s+/g, '');
   const resultDiv = document.getElementById("result");
-  const match = data.find(d => {
-    const full = `${d.시도명} ${d.시군구명} ${d.관리구역명}`;
+  const match = currentList.find(d => {
+    const full = \`\${d.시도명}\${d.시군구명}\${d.관리구역명}\`.replace(/\s+/g, '');
     return full === input;
   });
   resultDiv.innerHTML = match ? `
-    <strong>📍 ${match.시도명} ${match.시군구명} ${match.관리구역명}</strong><br><br>
-    🏠 배출장소: ${match.배출장소}<br>
-    🗑️ 생활쓰레기: ${match.생활쓰레기배출방법} (${match.생활쓰레기배출요일})<br>
-    🍱 음식물쓰레기: ${match.음식물쓰레기배출방법} (${match.음식물쓰레기배출요일})<br>
-    ♻️ 재활용품: ${match.재활용품배출방법} (${match.재활용품배출요일})<br>
-    🚫 미수거일: ${match.미수거일}<br>
-    ☎️ 문의: ${match.관리부서전화번호}<br>
+    <strong>📍 \${match.시도명} \${match.시군구명} \${match.관리구역명}</strong><br><br>
+    🏠 배출장소: \${match.배출장소}<br>
+    🗑️ 생활쓰레기: \${match.생활쓰레기배출방법} (\${match.생활쓰레기배출요일})<br>
+    🍱 음식물쓰레기: \${match.음식물쓰레기배출방법} (\${match.음식물쓰레기배출요일})<br>
+    ♻️ 재활용품: \${match.재활용품배출방법} (\${match.재활용품배출요일})<br>
+    🚫 미수거일: \${match.미수거일}<br>
+    ☎️ 문의: \${match.관리부서전화번호}<br>
   ` : "❗ 일치하는 지역 정보를 찾을 수 없습니다.";
 }
